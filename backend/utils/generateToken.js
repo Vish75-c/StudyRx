6d2938fb6ad1fb6a9154c1e161ad47feecc2af87
Vish75-c <vishgola191@gmail.com>
2026-04-12 22:04:46 +0530

    TokenEnvMisMatch

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,9 +2,7 @@ import jwt from "jsonwebtoken"
 import dotenv from "dotenv";
 dotenv.config();
 const generateToken=(userId)=>{
-    console.log("IN")
-    console.log(process.env.JWT_SECRET)
-    return jwt.sign({id:userId},"2463",{
+    return jwt.sign({id:userId},process.env.JWT_SECRET,{
         expiresIn:"7d",
     })
 }
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  baseURL: `https://medic-ai-1.onrender.com/api`,
 });
 
 // Attach token to every request
